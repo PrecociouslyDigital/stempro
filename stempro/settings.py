4cbad694fa895c93f09bcfc1353ad804e321bff8
Jack Yuan <ylong667@gmail.com>
2019-09-01 22:51:05 -0700

    change settings

--- stempro/settings.py
+++ stempro/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'D:/home/stempro/wwwroot/static'
+#os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'thanks'
