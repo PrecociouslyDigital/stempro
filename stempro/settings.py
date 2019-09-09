156c4412a0db57d8f0d545d4a3e5d4785d167c20
Jack Yuan <ylong667@gmail.com>
2019-09-08 18:55:50 -0700

    connect azure database

--- stempro/settings.py
+++ stempro/settings.py
@@ -79,16 +79,32 @@ WSGI_APPLICATION = 'stempro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'stemprodb',
+#        'USER':'postgres',
+#        'PASSWORD':'stempro',
+#        'HOST': 'localhost',
+#        'PORT': '5455',
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'stemprodb',
-        'USER':'postgres',
-        'PASSWORD':'stempro',
-        'HOST': 'localhost',
-        'PORT': '5455',
+        'USER': 'stemproadm@stempro',
+        'PASSWORD':'Stempro!',
+        'HOST': 'stempro.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
+
 
 
 # Password validation
