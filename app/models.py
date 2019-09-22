26bcec8c837d4f46f526a2e023c45190bca903a7
Jack Yuan <ylong667@gmail.com>
2019-09-22 16:59:31 -0700

    many update

--- app/models.py
+++ app/models.py
@@ -79,11 +79,16 @@ class Program(models.Model):
 
 
 class RegisterActive(models.Model):
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    phone_number = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=100, null=True)
     active_name = models.CharField(max_length=200)
     who_register = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     register_date = models.DateField(default=date.today)
     is_paid = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return self.who_register
