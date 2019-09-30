279b6fee4e609f80e09ccb42cca3efbb4e237dd2
Jack Yuan <ylong667@gmail.com>
2019-09-30 14:41:09 -0700

    update

--- app/views.py
+++ app/views.py
@@ -324,9 +324,3 @@ class VolunteerView(TemplateView):
         form = self.form_class()
         return render(request, self.template_name, { 'form': form })
 
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-              # <process form cleaned data>
-            return HttpResponseRedirect('/success/')
-        return render(request, self.template_name, {'form': form})
